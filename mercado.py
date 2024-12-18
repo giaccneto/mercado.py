4ceeee150cb056f8dbc3679f85c437299f4ad339
Giacomo Accardi Neto <44924062+giaccneto@users.noreply.github.com>
2024-12-17 20:54:06 -0400

    Update mercado.py

--- mercado.py
+++ mercado.py
@@ -1,15 +1,14 @@
 produtos = []
 carrinho = []
 
+
 def cadastrar_produto():
     nome_produto = input("Digite o nome do produto: ")
-    marca_produto = input("Digite a marca do produto: ")
     preco_produto = float(input("Digite o preço do produto: "))
     quantidade_produto = int(input("Insira a quantidade do produto ne estoque :"))
 
     produto = {
         "produto": nome_produto,
-        "marca": marca_produto,
         "preco": preco_produto,
         "qunatidade": quantidade_produto
 
@@ -21,22 +20,28 @@ def cadastrar_produto():
 def produtos_estoque():
     for produto in produtos:
         for key, value in produto.items():
-            print(f'{key}: {value}')
+            print(f'{key}: {value}\n')
 
 
 def adicionar_no_carrinho():
     selecionar_produto = input("Digite o nome do produto desejado: ")
+    qtd = int(input("Digite a quantidade desejada: "))
+
     for produto in produtos:
-        if produto['produto'].lower() == selecionar_produto.lower():
-            carrinho.append(produto)
+
+        if produto["produto"].lower() == selecionar_produto.lower():
+            carrinho.append({"produto": produto["produto"], "quantidade": qtd, "preco": produto["preco"]})
             print("Produto adicionado ao carrinho...")
         else:
             print("Produto não encontrado!")
+
+    print(carrinho)
 
 def listar_produtos_carrinho():
     for produto in carrinho:
         for key, value in produto.items():
             print(f'{key}:{value}')
+
 
 def remover_produto_carrinho():
     selecionar_produto = input("Digite o nome do produto desejado: ")
@@ -47,8 +52,8 @@ def remover_produto_carrinho():
         else:
             print("Produto não encontrado!")
 
-def finalizer_compra():
 
+def finalizer_compra():
     resposta = input("Deseja finalizar a compra?(SIM/NAO): ")
     if resposta.lower() == 'sim':
         forma_pagamento = input("Qual a forma de pagamento? (Dinheiro, Cartão ou PIX): ")
@@ -60,25 +65,39 @@ def finalizer_compra():
             print("Pagamento efetuado com PIX, obrigado pela preferencia!")
         else:
             print("Indique uma forma de pagamento corresta!")
-    
 
 
+def menu():
+    while True:
+        opcao = input("Escolha uma opção: "
+                      "\n1 - CADASTRAR PRODUTO: "
+                      "\n2 - VER PRODUTOS DISPONIVEIS: "
+                      "\n3 - ADICIONAR UM PRODUTO NO CARINHO: "
+                      "\n4 - REMOVER UM PRODUTO DO CARRINHO: "
+                      "\n5 - FINALIZAR A COMPRA: "
+                      "\n0 - PARA SAIR: ")
+        if opcao == "0":
+            print("Saindo do sistema...")
+            break
+        elif opcao == "1":
+            cadastrar_produto()
+        elif opcao == "2":
+            produtos_estoque()
+        elif opcao == "3":
+            adicionar_no_carrinho()
+        elif opcao == "4":
+            remover_produto_carrinho()
+            #listar_produtos_carrinho()
+        elif opcao == "5":
+            finalizer_compra()
+        else:
+            print("Entrar uma opção valida!")
 
-
-cadastrar_produto()
-produtos_estoque()
-adicionar_no_carrinho()
-listar_produtos_carrinho()
-remover_produto_carrinho()
-finalizer_compra()
-
-
-
-#if __name__ == "__main__":
-#    menu()
+if __name__ == "__main__":
+    menu()
 
 '''
-Cadastrar novos produtos
+Cadastrar novos produtos    ok
 
 ○ Listar os produtos em estoque     OK
 
